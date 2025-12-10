510f512d2d41499ef7b742dd37fb8426ddd9be95
CotikCommunist <kuvaev1224@gmail.com>
2025-12-10 16:14:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 $(document).ready(function () {
 
     $("#form1").on("submit", function (e) {
-        e.preventDefault(); // Не перезагружаем страницу
-
+        e.preventDefault();
+        
         let name = $("#name1").val().trim();
         let email = $("#email1").val().trim();
         let message = $("#message1").val().trim();
-
-        // Проверка имени
+        
         if (name.length < 3) {
             alert("Имя должно содержать минимум 3 символа");
             return;
         }
-
-        // Проверка почты через RegExp
         const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailReg.test(email)) {
             alert("Введите корректную почту");
             return;
         }
-
-        // AJAX-запрос
         $.ajax({
             url: "http://127.0.0.1",
             method: "POST",
