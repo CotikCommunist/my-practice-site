38c416d802361cf7891a14c088c183fafee767ad
CotikCommunist <kuvaev1224@gmail.com>
2025-12-11 18:55:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,54 @@
 $(document).ready(function () {
 
-    // Форма
-    $("#form1").on("submit", function (event) {
-        event.preventDefault();
+    // Форма 
+    $("#form1").on("submit", function (e) {
+        e.preventDefault();
 
         let name = $("#name1").val().trim();
         let email = $("#email1").val().trim();
         let message = $("#message1").val().trim();
-        // Проверка имени
+
         if (name.length < 3) {
-            alert("Имя слишком короткое");
+            alert("Имя должно содержать минимум 3 символа");
             return;
         }
-        // Проверка email
-        let emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        const emailReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailReg.test(email)) {
-            alert("Некорректная почта");
+            alert("Введите корректную почту");
             return;
         }
-        $.ajax({
-            url: "http://127.0.0.1",
-            method: "POST",
-            data: {
-                name: name,
-                email: email,
-                message: message
-            },
-            success: function () {
-                alert("Запрос отправлен!");
-            },
-            error: function () {
-                alert("Запрос ушёл успешно (сервер локально может быть выключен)");
-            }
-        });
+
+        alert("Запрос отправлен!");
     });
+
     // Загрузка отзывов
     function loadReviews() {
         $.getJSON("reviews.json", function (data) {
 
-            let block = $("#reviewsList");
-            block.empty();
+            let container = $("#reviewsList");
+            container.empty();
 
-            data.forEach(function (rev) {
-                let html = 
-                    <div class="review">
-                        <h4>${rev.name}</h4>
-                        <p>${rev.text}</p>
+            data.forEach(r => {
+
+                let item = 
+                <div class="col-md-4">
+                    <div class="p-3 shadow-sm rounded bg-white">
+                        <h5>${r.name}</h5>
+                        <p class="text-muted">${r.text}</p>
                     </div>
+                </div>
                 ;
-                block.append(html);
+
+                container.append(item);
             });
 
         }).fail(function () {
-            console.log("Ошибка: reviews.json не найден или в нём ошибка");
+            console.log("Ошибка загрузки reviews.json");
         });
     }
-    // Загружаю отзывы
+
     loadReviews();
+
+
 });
